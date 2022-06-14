3ebc5112e1910f2e7fa80cb1686f9e8b85e06ce0
7bomb <107449205+7bomb@users.noreply.github.com>
2022-06-14 12:24:26 +0800

    asdaga

--- main.go
+++ main.go
@@ -203,7 +203,7 @@ func init() {
 
 	// 通过代码写死的方式添加主人账号
 	sus = append(sus, 2971533238)
-	// sus = append(sus, 87654321)
+	sus = append(sus, 2971533238)
 
 	if *runcfg != "" {
 		f, err := os.Open(*runcfg)
